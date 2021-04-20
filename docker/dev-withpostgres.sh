1204eb81224d5a20ec888a0c26642af08935a9a7
Michael Connors <michael.connors@gmail.com>
2021-04-20 12:44:39 +0100

    added fixes for running with postgres instead of mysql

--- docker/dev-withpostgres.sh
+++ docker/dev-withpostgres.sh
@@ -5,13 +5,13 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR && \
   COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose \
-    -f docker-compose.yml \
+    -f docker-compose-postgres.yml \
     -f docker-compose.postgresoverride.yml \
     -f docker-compose.dev.yml \
     pull \
 && \
   COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose -p datahub \
-    -f docker-compose.yml \
+    -f docker-compose-postgres.yml \
     -f docker-compose.postgresoverride.yml \
     -f docker-compose.dev.yml \
     up
